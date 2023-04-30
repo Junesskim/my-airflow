f33e2bd28ee2c134eb5dec84209077b98e4e6595
Junesskim <aktpfkxl9891@naver.com>
2023-04-30 14:46:15 +0900

    Change aa.py

--- aa.py
+++ aa.py
@@ -1,21 +1,22 @@
 from urllib import request
+import datetime
 
-import airflow from airflow
-import DAG from airflow.operators.python import PythonOperator
+import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+target_date = datetime.datetime(2021, 1, 1, tzinfo=datetime.timezone.utc)
 
 dag = DAG(
     dag_id = "first_dag",
-    start_date = '2021-01-01',
-    schedule_interval = None,
+    start_date = target_date,
+    schedule = None,
 )
 
 def _get_data(execution_date):
     year, month, day = execution_date.timetuple()
-    url = (
-        "http://127.0.0.1:5000/"
-        f"{year}"/f"{month}"/f"{day}"
-    )
-    output_path = "./airflow/dags"
+    url = ("host.docker.internal:5000/" f"{year}"/f"{month}"/f"{day}")
+    output_path = "./airflow/dags" # <- ./airflow 디렉토리가 ?
     request.urlretrieve(url, output_path)
 
 get_data = PythonOperator(
